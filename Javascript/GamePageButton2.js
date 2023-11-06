e4d5f4ff857eb83da316bea6000c5553a31297dc
Dreadnoutishe <124653689+Dreadnoutishe@users.noreply.github.com>
2023-11-06 15:29:01 +0600

    ss

---
+++ Javascript/GamePageButton2.js
@@ -0,0 +1,14 @@
+const button = document.getElementById("animeButton2");
+
+    button.addEventListener("click", () => {
+        // Add CSS animation class to the button
+        button.style.animation = "scale 2.0s";
+        
+        // Play a sound (replace 'sound.mp3' with the actual sound file)
+        const audio = new Audio("assets/him.mp3");
+        audio.play();
+        // Redirect to another HTML page (replace 'otherpage.html' with the actual URL)
+        setTimeout(() => {
+            window.location.href = "puzzle.html";
+        }, 7000);
+    });
